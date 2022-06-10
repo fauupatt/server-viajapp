ee1b53109d6991cc4f7311608350085344f062fd
Fausto Patt <fausto.patt@outlook.com>
2022-06-10 19:14:30 -0300

    Subo

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', function(req, res){
-    res.send('Hola mundo!')
+
+app.get('/', function (req, res) {
+  res.send('Hola mundo')
 })
-app.listen(4000)
+
+app.listen(3000)
